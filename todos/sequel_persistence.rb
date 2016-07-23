d84687064ca37544a2159dd18dcbd31f16ee1ac5
floalex <flo.liang@my.com>
2016-07-23 00:11:56 -0700

    Add sequelpersistence codes

--- todos/sequel_persistence.rb
+++ todos/sequel_persistence.rb
@@ -6,11 +6,6 @@ class SequelPersistence
     @db.loggers << logger
   end
 
-  def query(statement, *params)
-    @logger.info "#{statement}: #{params}"
-    @db.exec_params(statement, params)
-  end
-
   def find_list(id)
     # all_lists.where(lists__id: id).first
     all_lists.first(lists__id: id)
@@ -28,51 +23,36 @@ class SequelPersistence
   end
 
   def create_new_list(list_name)
-    sql = "INSERT INTO lists (name) VALUES ($1)"
-    query(sql, list_name)
+    @db[:lists].insert(name: list_name)
   end
 
   def delete_list(id)
     # Deleting the list would fail due to the foreign key between todos.list_id and lists.id
-    query("DELETE FROM todos WHERE list_id = $1", id)
-    query("DELETE FROM lists WHERE id = $1", id)
+    @db[:todos].where(list_id: id).delete
+    @db[:lists].where(id: id).delete
   end
 
   def update_list_name(id, new_name)
-    sql = "UPDATE lists SET name = $1 WHERE id = $2"
-    query(sql, new_name, id)
+    @db[:lists].where(id: id).update(name: new_name)
   end
 
   def create_new_todo(list_id, todo_name)
-    sql = "INSERT INTO todos (list_id, name) VALUES ($1, $2)"
-    query(sql, list_id, todo_name)
+    @db[:todos].insert(list_id: list_id, name: todo_name)
   end
 
   def delete_todo_from_list(list_id, todo_id)
-    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
-    query(sql, todo_id, list_id)
+    @db[:todos].where(list_id: list_id, id: todo_id).delete
   end
 
   def update_todo_status(list_id, todo_id, new_status)
-    sql = "UPDATE todos SET completed = $1 WHERE id = $2 AND list_id = $3"
-    query(sql, new_status, todo_id, list_id)
+    @db[:todos].where(list_id: list_id, id: todo_id).update(completed: new_status)
   end
 
   def mark_all_todos_as_completed(list_id)
-    sql = "UPDATE todos SET completed = true where list_id = $1"
-    query(sql, list_id)
+    @db[:todos].where(list_id: list_id).update(completed: true)
   end
 
   def find_todos_for_list(list_id)
     @db[:todos].where(list_id: list_id)
-  end
-
-  private
-
-  def tuple_to_list_hash(tuple)
-    { id: tuple["id"].to_i, 
-      name: tuple["name"], 
-      todos_count: 0, 
-      todos_remaining_count: 0 }
   end
 end
